36b6af7924b9d0e73d01b5377e80ff0b61cbb750
RohonRay123 <50560353+RohonRay123@users.noreply.github.com>
2023-12-03 23:43:17 -0700

    Update Trim.sv

--- rtl/Trim.sv
+++ rtl/Trim.sv
@@ -1,11 +1,11 @@
 // Code your design here
 module Trim #(parameter SIZE=5)
-  (input [31:0] FFTArray[0:(2*(SIZE-1))][0:(2*(SIZE-1))] , output [31:0] FFTTriming[0:(SIZE-1)][0:(SIZE-1)],en,clk,reset)
+  (input [31:0] FFTArray[0:(2*(SIZE-1))][0:(2*(SIZE-1))] , output logic [31:0] FFTTriming[0:(SIZE-1)][0:(SIZE-1)],input en,input clk,input reset);
   logic [31:0] FFTTriming1[0:(SIZE-1)][0:(SIZE-1)];
-  logic [31:0] trim_first;
-  logic [31:0] trim_last;
-  assign  trim_last=(SIZE-1)/2;
-  assign trim_first=(SIZE-1)-((SIZE-1)/2);
+ // logic [31:0 ]trim_first;
+ // logic [31:0] trim_last;
+  parameter  trim_last=(SIZE-1)/2;
+  parameter trim_first=(SIZE-1)-((SIZE-1)/2);
   genvar row;
   genvar col;
   int row1;
@@ -16,12 +16,12 @@ module Trim #(parameter SIZE=5)
           assign FFTTriming1[row][col]=FFTArray[row-trim_first][col-trim_first];
         end 
       end 
-  always @(posedge)
+  always @(posedge clk)
     begin 
       if(reset) begin 
         for(row1=0;row1<(SIZE);row1=row1+1) begin 
           for(col1=0;col1<(SIZE);col1=col1+1) begin 
-            FFTTriming[row1][col1]<='d0;
+            FFTTriming[row1][col1]<=FFTArray[row1][col1];
           end 
         end
       end 
