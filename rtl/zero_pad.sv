f3a3029562c4b243a304d6cc7b1beebd992ea4a5
RohonRay123 <50560353+RohonRay123@users.noreply.github.com>
2023-12-03 22:57:25 -0700

    Update zero_pad.sv

--- rtl/zero_pad.sv
+++ rtl/zero_pad.sv
@@ -1,27 +1,35 @@
 // Code your design here
-module zero_pad #(parameter SIZE=5) (input [31:0] array1[0:(SIZE-1)][0:(SIZE-1)],output [31:0] array2[0:(2*(SIZE-1))][0:(2*(SIZE-1))],clk,en,reset)
+`timescale 1ns/1ps
+module zero_pad #(parameter SIZE=5) (input [31:0] array1[0:(SIZE-1)][0:(SIZE-1)],output logic [31:0] array2[0:(2*(SIZE-1))][0:(2*(SIZE-1))],input clk,input en,input reset);
  genvar row;
  genvar col;
   logic [31:0] array2b[0:(2*(SIZE-1))][0:(2*(SIZE-1))];
-  for(row=0;row<SIZE;row=row+1) begin 
-    for(col=0;col<SIZE;col=col+1) begin 
-      assign array2b[row][col]=array1[row][col];
+  
+
+  for( row=0;row<SIZE;row=row+1) begin 
+    for( col=0;col<((2*(SIZE-1))+1);col=col+1) begin 
+   //   assign array2b[row][col]=array1[row][col];
+      assign array2b[row][col]=(col<SIZE) ? array1[row][col]:'d0;
       
     end 
   end 
   for(row=SIZE;row<((2*SIZE)-1);row=row+1) begin
-   for(col=SIZE;col<((2*SIZE)-1;col=col+1) begin 
-     assign array2b[row][col]<='d0;
+    for(col=0;col<((2*SIZE)-1);col=col+1) begin 
+   ///$display("This is array2[%0d][%0d]=%0d",row,col,array2[row][col]);
+
+    assign array2b[row][col]='d0;
     end 
   end 
+
+       
   int row1;
   int col1;
   always @(posedge clk) 
   begin 
   if(reset) begin 
-   for(row1=0;row1<SIZE;row1=row1+1) begin 
-    for(col1=0;col1<SIZE;col1=col1+1) begin 
-     array2[row1][col1]<='d0;
+    for(row1=0;row1<((2*(SIZE-1))+1);row1=row1+1) begin 
+      for(col1=0;col1<((2*(SIZE-1))+1);col1=col1+1) begin 
+        array2[row1][col1]<='d0;
      end 
     end 
       end
